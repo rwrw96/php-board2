bb650a483bc51bbf7a7e883909eaebeddc681872
rwrw96 <kazuki137@gamil.com>
2021-08-07 18:25:16 +0900

    PDO修正

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-    $db = new PDO ('mysql:dbname=boarddb; host=localhost; charset=utf8', 'root', 'root');
+    $db = new PDO ('mysql:dbname=boarddb; host=mydb.cle5jiuhhe8w.ap-northeast-1.rds.amazonaws.com; charset=utf8', 'root', 'kazuki108');
 } catch (PDOException $e) {
     echo $e -> getMessage();
     exit();
