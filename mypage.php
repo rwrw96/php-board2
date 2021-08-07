af3ce4399aff2b01c446c2adcdbabae5678dca0b
rwrw96 <kazuki137@gamil.com>
2021-08-07 15:58:23 +0900

    [add]ログアウト機能実装

--- mypage.php
+++ mypage.php
@@ -24,6 +24,10 @@ $user_id = $user['id'];
 	<p>メールアドレス：<?php echo htmlspecialchars($user['email'], ENT_QUOTES); ?></p>
 	<p><img src="user_images/<?php echo htmlspecialchars($user['image'], ENT_QUOTES); ?>" alt="写真" width=230 height=200></p>
 
-	<a href="edit.php?id=<?php echo $user_id ?>">編集する</a>
+	<a href="index.php">投稿一覧へ</a>
+	<?php if($_GET['id'] == $_SESSION['user']['id']): ?>
+		<a href="edit.php?id=<?php echo $user_id ?>">編集する</a>
+		<a href="logout.php">ログアウトする</a>
+	<?php endif; ?>
 </body>
 </html>
